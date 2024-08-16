7f06870af171409642b855c6b43d00b4375889d5
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-08-16 15:26:19 -0300

    add files before making shit

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -25,6 +25,9 @@ return {
       require("lspconfig").tailwindcss.setup {
 	capabilities = capabilities
       }
+      require("lspconfig").ruby_lsp.setup {
+	capabilities = capabilities
+      }
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
