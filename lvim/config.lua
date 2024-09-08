0fc4dd3f21d0be53d6e8b7a6fc78ab3126a4fc98
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-09-08 01:17:32 -0300

    update .config

--- lvim/config.lua
+++ lvim/config.lua
@@ -6,25 +6,37 @@ lvim.format_on_save.enabled = true
 lvim.builtin.lualine.options.theme = "catppuccin"
 lvim.colorscheme = "catppuccin"
 
+-- Open the terminal in a horizontal split
+lvim.builtin.terminal.open_mapping = "<C-t>"
+lvim.builtin.terminal.direction = "horizontal"
+
 lvim.plugins = {
   {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
-      flavour = "mocha",
+      flavour = "frappe",
       transparent_background = true
     },
   },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
 
-table.insert(lvim.plugins, {
-  "zbirenbaum/copilot-cmp",
-  event = "InsertEnter",
-  dependencies = { "zbirenbaum/copilot.lua" },
-  config = function()
-    vim.defer_fn(function()
-      require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-    end, 100)
-  end,
-})
+table.insert(lvim.plugins,
+  {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+      end, 100)
+    end,
+  }
+)
